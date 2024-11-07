0abf4152271b0e2302a50ae05c449a122677ddd9
craz1gre0 <120773934+craz1gre0@users.noreply.github.com>
2024-11-08 01:32:01 +0800

    Add files via upload

---
+++ video.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const thevideo = document.getElementById("thevideo");
+    const othervideo = document.getElementById("othervideo");
+    const h1 = document.getElementById("h1");
+    const p1 = document.getElementById("p1");
+
+    const backh1 = h1.textContent;
+    const backp1 = p1.textContent;
+
+    //p
+    function playoth() {
+
+        thevideo.style.display = "none";
+        othervideo.style.display = "block";
+        
+        othervideo.muted = false;
+        othervideo.play();
+
+        h1.textContent = " ";
+        p1.textContent = " ";
+    }
+
+    //c
+    thevideo.addEventListener("click", playoth);
+    h1.addEventListener("click", playoth);
+    p1.addEventListener("click", playoth);
+
+    //end
+    othervideo.addEventListener("ended", function() {
+        
+        othervideo.muted = true;
+        othervideo.style.display = "none";
+        thevideo.style.display = "block";
+        
+        h1.textContent = backh1;
+        p1.textContent = backp1;
+        thevideo.play();
+    });
+});
