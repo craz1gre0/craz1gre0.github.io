3a57ad081636a1eca631b7bc66702fa3f69cfb17
craz1gre0 <120773934+craz1gre0@users.noreply.github.com>
2024-11-08 20:52:59 +0800

    Update video.js

--- video.js
+++ video.js
@@ -8,19 +8,17 @@ document.addEventListener("DOMContentLoaded", function() {
     const backh1 = h1.textContent;
     const backp1 = p1.textContent;
 
-    // 檢查是否為行動裝置
     function isMobile() {
         return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
     }
 
-    // 檢查是否為橫向模式且在行動裝置上
     function isportrait() {
         return isMobile() && window.matchMedia("(orientation:  portrait)").matches;
     }
 
     // s
     function playoth() {
-        // 如果是行動裝置的橫向模式，則執行影片控制
+        
         if (isportrait()) {
             thevideo.style.display = "none";
             mobilevideo.style.display = "block";
